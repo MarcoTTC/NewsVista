f7b3ba83726b89d3a62211b3a4a607edfb88566c
Marco Coelho <marco.tulio.todeschini.coelho@gmail.com>
2024-07-10 21:47:32 -0300

    Adding a global constants file

---
+++ app/src/main/java/br/com/marcottc/newsvista/util/Constants.kt
@@ -0,0 +1,9 @@
+package br.com.marcottc.newsvista.util
+
+// If this val is an empty string, you must access NY Times developer portal at
+// "https://developer.nytimes.com/", register yourself an account, create an app,
+// retrieve the app id for your app, and add it in this val
+const val nyTimesAppId: String = ""
+
+// Base url for the NY Times Dev API
+const val BASE_URL: String = ""
